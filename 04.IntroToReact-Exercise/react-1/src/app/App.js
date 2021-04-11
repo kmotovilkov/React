864eb04f4d158d7d45046aecb8a056ae41b65e56
kmotovilkov <monyllakk@abv.bg>
2021-04-11 15:07:39 +0300

    update

--- 04.IntroToReact-Exercise/react-1/src/app/App.js
+++ 04.IntroToReact-Exercise/react-1/src/app/App.js
@@ -1,53 +1,55 @@
 import logo from '../logo.svg';
 import '../App.css';
-import './style/app.css'
+import './style/app.css';
+import Contacts from './contacts.json';
+
+const showContacts = (data) => (
+    <div className="contact" key={data.email} data-id="id">
+        <span className="avatar small">&#9787;</span>
+        <span className="title">{data.firstName} {data.lastName}</span>
+    </div>);
+
+const contactList = [];
+const renderList = () => {
+    for (let contact of Contacts) {
+        contactList.push(showContacts(contact));
+    }
+    return contactList;
+};
 
 function App() {
-  return (
-      <div className="container">
-        <header>&#9993; Contact Book</header>
-        <div id="book">
-          <div id="list">
-            <h1>Contacts</h1>
-            <div className="content">
-              <div className="contact" data-id="id">
-                <span className="avatar small">&#9787;</span>
-                <span className="title">Ivan Ivanov</span>
-              </div>
-
-              <div className="contact" data-id="id">
-                <span className="avatar small">&#9787;</span>
-                <span className="title">Jordan Kirov</span>
-              </div>
-
-              <div className="contact" data-id="id">
-                <span className="avatar small">&#9787;</span>
-                <span className="title">Maria Petrova</span>
-              </div>
-            </div>
-          </div>
-          <div id="details">
-            <h1>Details</h1>
-            <div className="content">
-              <div className="info">
-                <div className="col">
-                  <span className="avatar">&#9787;</span>
+    return (
+        <div className="container">
+            <header>&#9993; Contact Book</header>
+            <div id="book">
+                <div id="list">
+                    <h1>Contacts</h1>
+                    <div className="content">
+                        {renderList()}
+                    </div>
                 </div>
-                <div className="col">
-                  <span className="name">Ivan</span>
-                  <span className="name">Ivanov</span>
+                <div id="details">
+                    <h1>Details</h1>
+                    <div className="content">
+                        <div className="info">
+                            <div className="col">
+                                <span className="avatar">&#9787;</span>
+                            </div>
+                            <div className="col">
+                                <span className="name">Ivan</span>
+                                <span className="name">Ivanov</span>
+                            </div>
+                        </div>
+                        <div className="info">
+                            <span className="info-line">&phone; 0887 123 456</span>
+                            <span className="info-line">&#9993; i.ivanov@gmail.com</span>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div className="info">
-                <span className="info-line">&phone; 0887 123 456</span>
-                <span className="info-line">&#9993; i.ivanov@gmail.com</span>
-              </div>
             </div>
-          </div>
+            <footer>Contact Book SPA &copy; 2017</footer>
         </div>
-        <footer>Contact Book SPA &copy; 2017</footer>
-      </div>
-  );
+    );
 }
 
 export default App;
