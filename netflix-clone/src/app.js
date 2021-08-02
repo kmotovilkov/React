e2c8a2f446bb26c08a6277d622f78da5b0e05837
kmotovilkov <monyllakk@abv.bg>
2021-08-02 23:52:25 +0300

    update

--- netflix-clone/src/app.js
+++ netflix-clone/src/app.js
@@ -1,10 +1,12 @@
 import React from "react";
+import {FaqsContainer} from "./containers/faqs";
 import {JumbotronContainer} from "./containers/jumbotron";
 import {FooterContainer} from "./containers/footer";
 
 export default function App() {
     return <>
         <JumbotronContainer/>
+        <FaqsContainer/>
         <FooterContainer/>
     </>;
 
